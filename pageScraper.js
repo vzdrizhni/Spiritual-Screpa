72b319e9fdcea77b1d9d0cc8f91920bda6034136
unknown <vzdrizhni@gmail.com>
2021-03-01 20:36:18 +0500

    Modify urls for getting data in grams

--- pageScraper.js
+++ pageScraper.js
@@ -50,15 +50,47 @@ const scraperObject = {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.anotherUrl}...`);
         await page.goto(this.anotherUrl);
-        await page.waitFor(5000);
-        await page.waitForSelector('.nrbutton');        
+        await page.waitForSelector('#foodlistranking');
+        
+        await page.$eval('#datasourcenr',
+            e => {
+                e.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                })
+            })
+        await page.waitFor(3000);
         await page.click('.nrbutton');
-        await page.waitFor(5000);
+        await page.waitFor(3000);
         let urls = await page.$$eval('#foodlistranking > li', links => {
             links = links.map(el => el.querySelector('div > a').href)
             return links;
         })
-        console.log(urls.length);
+        console.log(urls);
+
+        let pagePromise = (link) => new Promise(async(resolve, reject) => {
+            let dataObj = {};
+            let newPage = await browser.newPage();
+            await newPage.goto(link);
+            dataObj['bookTitle'] = await newPage.$eval('.product_main > h1', text => text.textContent);
+            dataObj['bookPrice'] = await newPage.$eval('.price_color', text => text.textContent);
+            dataObj['noAvailable'] = await newPage.$eval('.instock.availability', text => {
+                // Strip new line and tab spaces
+                text = text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, "");
+                // Get the number of stock available
+                let regexp = /^.*\((.*)\).*$/i;
+                let stockAvailable = regexp.exec(text)[1].split(' ')[0];
+                return stockAvailable;
+            });
+            dataObj['imageUrl'] = await newPage.$eval('#product_gallery img', img => img.src);
+            dataObj['bookDescription'] = await newPage.$eval('#product_description', div => div.nextSibling.nextSibling.textContent);
+            dataObj['upc'] = await newPage.$eval('.table.table-striped > tbody > tr > td', table => table.textContent);
+            resolve(dataObj);
+            await newPage.close();
+        });
+
+        urls = urls.map(item => item.substr(0, item.lastIndexOf("/")).concat("/100g"));
+        console.log(urls);
     }
 
 }
